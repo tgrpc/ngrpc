63bda90f666194bf38d8eaa76aa8d896b9712f79
shiyongbin <shiyongbin@ezbuy.com>
2018-04-17 14:01:31 +0800

    grpc:2080

--- grpcurl.sh
+++ grpcurl.sh
@@ -1,9 +1,13 @@
-grpcurl -insecure=true -protoset helloworld.protoset localhost:80 list helloworld.Greeter
+grpcurl -insecure=true -protoset helloworld.protoset localhost:2080 list helloworld.Greeter
 
-grpcurl -insecure=true -protoset helloworld.protoset localhost:80 describe helloworld.Greeter
+grpcurl -insecure=true -protoset helloworld.protoset localhost:2080 describe helloworld.Greeter
 
-grpcurl -insecure=true -d '{"name":"ngrpc"}' -protoset helloworld.protoset localhost:80 helloworld.Greeter/SayHello
+grpcurl -insecure=true -d '{"name":"ngrpc"}' -protoset helloworld.protoset localhost:2080 helloworld.Greeter/SayHello
 
-grpcurl -insecure=true -d '{"name":"ngrpc"}' -H 'cookie:customer=abcdefg,admin_customer=1234567' -H 'region:CH' -protoset helloworld.protoset localhost:80 helloworld.Greeter/SayHello
+grpcurl -insecure=true -d '{"name":"ngrpc"}' -H 'cookie:customer=abcdefg,admin_customer=1234567' -H 'region:CH' -protoset helloworld.protoset localhost:2080 helloworld.Greeter/SayHello
 
-echo '{"name":"tgrpc-prototool"}' | prototool -H 'cookie:customer=abcdefg,admin_customer=1234567;region:CH' grpc helloworld 0.0.0.0:80 helloworld.Greeter/SayHello -
+echo '{"name":"tgrpc-prototool"}' | prototool -H 'cookie:customer=abcdefg,admin_customer=1234567;region:CH' grpc helloworld 0.0.0.0:2080 helloworld.Greeter/SayHello -
+
+
+
+grpcurl -insecure=true -d '{"name":"ngrpc"}' localhost:2080 helloworld.Greeter/SayHello
